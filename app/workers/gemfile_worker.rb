a52fc4763181622840dc1bee4796f6d50025dc5d
Daniel Moore <yahivin@gmail.com>
2013-05-18 00:09:25 -0700

    Adjusting worker delays

--- app/workers/gemfile_worker.rb
+++ app/workers/gemfile_worker.rb
@@ -1,10 +1,6 @@
 require_relative "base_worker"
 
 class GemfileWorker < BaseWorker
-  def delay
-    1.minutes
-  end
-
   def perform
     highlander do
       ActiveRecord::Base.uncached do
--- app/workers/update_worker.rb
+++ app/workers/update_worker.rb
@@ -1,10 +1,6 @@
 require_relative "base_worker"
 
 class UpdateWorker < BaseWorker
-  def delay
-    2.minutes
-  end
-
   def perform
     highlander do
       ActiveRecord::Base.uncached do
