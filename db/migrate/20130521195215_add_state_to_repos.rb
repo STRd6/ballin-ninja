bed882fbf9df8f59a1eae903bb2995cf9adccca3
Daniel Moore <yahivin@gmail.com>
2013-05-21 16:57:52 -0700

    Updated migration to not initialize columns

--- db/migrate/20130521195215_add_state_to_repos.rb
+++ db/migrate/20130521195215_add_state_to_repos.rb
@@ -1,6 +1,6 @@
 class AddStateToRepos < ActiveRecord::Migration
   def change
-    add_column :repos, :state, :string, :null => false, :default => "", :limit => 16
+    add_column :repos, :state, :string, :limit => 16
 
     add_index :repos, :state
   end
