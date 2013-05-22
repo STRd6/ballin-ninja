29671b5e8c94556f0bf8c5101413292b8c81f241
Daniel Moore <yahivin@gmail.com>
2013-05-21 17:06:10 -0700

    Added constraints back

--- db/migrate/20130521195215_add_state_to_repos.rb
+++ db/migrate/20130521195215_add_state_to_repos.rb
@@ -1,6 +1,6 @@
 class AddStateToRepos < ActiveRecord::Migration
   def change
-    add_column :repos, :state, :string, :limit => 16
+    add_column :repos, :state, :string, :limit => 16, :null => false, :default => ""
 
     add_index :repos, :state
   end
