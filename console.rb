1d0624cbaadd4f0d47296dc730e2e2badc239aa5
Daniel Moore <yahivin@gmail.com>
2013-05-22 18:19:08 -0700

    Adding workers to console

--- console.rb
+++ console.rb
@@ -5,6 +5,11 @@ require "./models"
 require "./app/models/api_token"
 require "logger"
 require "pry"
+require "sidekiq"
+
+Dir[File.dirname(__FILE__) + '/app/workers/*.rb'].each do |file|
+  require file
+end
 
 ActiveRecord::Base.logger = Logger.new(STDOUT)
 
